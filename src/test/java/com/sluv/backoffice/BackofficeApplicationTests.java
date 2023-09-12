4d8de8b8ff33d81c5d56a26dae2a63002abf7895
김보인 사원 Cloud Innovation Service팀 (charles1236@lgcns.com <charles1236@CNS.LGCNS.COM>
2023-09-12 19:02:25 +0900

    fix: delete SpringbootTest annotation

--- src/test/java/com/sluv/backoffice/BackofficeApplicationTests.java
+++ src/test/java/com/sluv/backoffice/BackofficeApplicationTests.java
@@ -3,7 +3,7 @@ package com.sluv.backoffice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class BackofficeApplicationTests {
 
 	@Test
