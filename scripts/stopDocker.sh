78fcab476e954aea530b9fa45d8c753865052e9c
김보인 사원 Cloud Innovation Service팀 (charles1236@lgcns.com <charles1236@CNS.LGCNS.COM>
2023-09-16 16:12:25 +0900

    fix stopDocker.sh

--- scripts/stopDocker.sh
+++ scripts/stopDocker.sh
@@ -1,1 +1,11 @@
-docker stop $(docker ps -a -q)
+#!/bin/bash
+
+# 컨테이너 개수 확인
+container_count=$(docker ps -q | wc -l)
+
+# 실행 중인 컨테이너가 하나도 없을 경우에만 실행
+if [ $container_count -gt 0 ]; then
+  docker stop $(docker ps -a -q)
+else
+  echo "No running containers to stop."
+fi
