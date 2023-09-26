85c6777f0f3adf8d9ce5014061bd76692f979c19
kdongk <ldh5081@naver.com>
2023-09-26 17:31:29 +0900

    [SBO-19] fix: UserRepositoryImpl findAllUserInfo 코드 수정

--- src/main/java/com/sluv/backoffice/domain/user/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/sluv/backoffice/domain/user/repository/impl/UserRepositoryImpl.java
@@ -1,19 +1,18 @@
 package com.sluv.backoffice.domain.user.repository.impl;
 
-import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sluv.backoffice.domain.user.dto.UserInfoDto;
 import com.sluv.backoffice.domain.user.entity.QUser;
 import com.sluv.backoffice.domain.user.entity.QUserReport;
 import com.sluv.backoffice.domain.user.entity.QUserReportStack;
-import com.sluv.backoffice.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepositoryCustom{
@@ -26,33 +25,26 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
     @Override
     public Page<UserInfoDto> findAllUserInfo(Pageable pageable) {
 
-        List<Tuple> tuples = jpaQueryFactory
-                .select(user, userReport.count(), userReportStack.countDistinct())
+        List<UserInfoDto> content = jpaQueryFactory
+                .select(Projections.constructor(UserInfoDto.class,
+                        user.nickname,
+                        user.profileImgUrl,
+                        user.userStatus,
+                        JPAExpressions.select(userReport.reported.id.count())
+                                .from(userReport)
+                                .where(userReport.reported.id.eq(user.id)),
+                        JPAExpressions.select(userReportStack.reported.id.count())
+                                .from(userReportStack)
+                                .where(userReportStack.reported.id.eq(user.id))
+                ))
                 .from(user)
-                .leftJoin(userReport).on(user.eq(userReport.reported))
-                .leftJoin(userReportStack).on(user.eq(userReportStack.reported))
-                .groupBy(user)
                 .orderBy(user.createdAt.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        List<UserInfoDto> userInfoDtos = tuples.stream()
-                .map(tuple -> {
-                    User u = tuple.get(user);
-                    Long reportCount = tuple.get(userReport.count());
-                    Long reportStackCount = tuple.get(userReportStack.countDistinct());
-                    return UserInfoDto.of(u, reportCount, reportStackCount);
-                })
-                .collect(Collectors.toList());
+        long total = jpaQueryFactory.from(user).fetch().size();
 
-        long totalCount = jpaQueryFactory
-                .selectFrom(user)
-                .leftJoin(userReport).on(user.eq(userReport.reported))
-                .leftJoin(userReportStack).on(user.eq(userReportStack.reported))
-                .groupBy(user.id)
-                .fetch().size();
-
-        return new PageImpl<>(userInfoDtos, pageable, totalCount);
+        return new PageImpl<>(content, pageable, total);
     }
 }
