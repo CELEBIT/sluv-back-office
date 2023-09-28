02e0076df9a15093b37c0adaaabce3f1f02b20aa
kdongk <ldh5081@naver.com>
2023-09-28 11:13:27 +0900

    [SBO-21] feat: 유저 상태 변경 기능 추가

--- src/main/java/com/sluv/backoffice/domain/user/controller/UserController.java
+++ src/main/java/com/sluv/backoffice/domain/user/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.sluv.backoffice.domain.user.controller;
 
 import com.sluv.backoffice.domain.user.dto.UserInfoDto;
+import com.sluv.backoffice.domain.user.enums.UserStatus;
 import com.sluv.backoffice.domain.user.service.UserService;
 import com.sluv.backoffice.global.common.response.ErrorResponse;
 import com.sluv.backoffice.global.common.response.PaginationResDto;
 import com.sluv.backoffice.global.common.response.SuccessDataResponse;
+import com.sluv.backoffice.global.common.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -13,9 +15,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,5 +38,22 @@ public class UserController {
         return ResponseEntity.ok().body(SuccessDataResponse.<PaginationResDto<UserInfoDto>>builder()
                 .result(userService.getAllUserInfo(pageable))
                 .build());
+    }
+
+    @Operation(
+            summary = "유저 상태 변경",
+            description = "ACTIVE, BLOCKED, DELETED, PENDING_PROFILE, PENDING_CELEB 중에서 유저의 상태를 변경한다."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "1000", description = "요청성공"),
+            @ApiResponse(responseCode = "5000", description = "서버내부 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "5001", description = "DB 에러", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @PutMapping("/{userId}/status/{status}")
+    public ResponseEntity<SuccessResponse> updateUserStatus(@PathVariable Long userId, @PathVariable UserStatus status) {
+        userService.updateUserStatus(userId, status);
+        return ResponseEntity.ok().body(
+                new SuccessResponse()
+        );
     }
 }
