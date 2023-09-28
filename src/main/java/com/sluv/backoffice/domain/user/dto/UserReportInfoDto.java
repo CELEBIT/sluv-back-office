40756e950f9a9a206e63263ba525dfa9033d9ee3
kdongk <ldh5081@naver.com>
2023-09-28 17:39:25 +0900

    [SBO-8] feat: UserReportInfoDto 컬럼에 swagger description 추가

--- src/main/java/com/sluv/backoffice/domain/user/dto/UserReportInfoDto.java
+++ src/main/java/com/sluv/backoffice/domain/user/dto/UserReportInfoDto.java
@@ -2,6 +2,7 @@ package com.sluv.backoffice.domain.user.dto;
 
 import com.sluv.backoffice.domain.user.enums.UserReportReason;
 import com.sluv.backoffice.global.common.enums.ReportStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,8 +19,11 @@ public class UserReportInfoDto {
     private Long reportedId;
     private String reportedNickname;
     private Long reportId;
+    @Schema(description = "신고 이유(enums)")
     private UserReportReason reportReason;
+    @Schema(description = "신고 상세 내용")
     private String content;
+    @Schema(description = "신고 접수 상태")
     private ReportStatus reportStatus;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
