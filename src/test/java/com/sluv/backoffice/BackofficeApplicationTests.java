43557ef22f4a21683f57a11a18371b282145544e
KJBig <kjgi73k@gmail.com>
2023-11-28 20:38:14 +0900

    feat: Test용 H2 데이터베이스 설정 추가

--- src/test/java/com/sluv/backoffice/BackofficeApplicationTests.java
+++ src/test/java/com/sluv/backoffice/BackofficeApplicationTests.java
@@ -3,11 +3,11 @@ package com.sluv.backoffice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
+ @SpringBootTest
 class BackofficeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	 @Test
+	 void contextLoads() {
+	 }
 
 }
