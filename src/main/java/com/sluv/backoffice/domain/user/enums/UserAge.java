18c97a25792f92b03b1dce77943c725a51c7c2e7
KJBig <kjgi73k@gmail.com>
2023-11-30 10:01:29 +0900

    SBO-31 feat : 사용자의 나이 Enum으로 관리
    - UserAge Enum 추가.

---
+++ src/main/java/com/sluv/backoffice/domain/user/enums/UserAge.java
@@ -0,0 +1,15 @@
+package com.sluv.backoffice.domain.user.enums;
+
+public enum UserAge {
+    UNKNOWN,
+    ZEROS,
+    TEENAGERS,
+    TWENTIES,
+    THIRTIES,
+    FORTIES,
+    FIFTIES,
+    SIXTIES,
+    SEVENTIES,
+    EIGHTIES,
+    NINETIES;
+}
