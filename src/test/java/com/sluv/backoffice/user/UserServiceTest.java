aea4e63ba99dbccd9236553367804a8d0144ef01
KJBig <kjgi73k@gmail.com>
2023-11-30 10:57:26 +0900

    SBO-31 feat : User의 연령대를 기준으로 분포도 조회 기능 테스트 코드

--- src/test/java/com/sluv/backoffice/user/UserServiceTest.java
+++ src/test/java/com/sluv/backoffice/user/UserServiceTest.java
@@ -8,6 +8,7 @@ import com.sluv.backoffice.domain.user.dto.UserCountByCategoryResDto;
 import com.sluv.backoffice.domain.user.dto.UserCountByCategoryResDto.UserCountByEachCategoryResDto;
 import com.sluv.backoffice.domain.user.entity.User;
 import com.sluv.backoffice.domain.user.enums.SnsType;
+import com.sluv.backoffice.domain.user.enums.UserAge;
 import com.sluv.backoffice.domain.user.enums.UserGender;
 import com.sluv.backoffice.domain.user.repository.UserRepository;
 import com.sluv.backoffice.domain.user.service.UserService;
@@ -86,6 +87,68 @@ public class UserServiceTest {
         assertThat(userCountByGender.getTotalCount()).isEqualTo(6);
         assertThat(woManUsers.get(0).getCount()).isEqualTo(3);
         assertThat(woManUsers.get(0).getPercent()).isEqualTo(50);
+
+    }
+
+    @DisplayName("사용자의 나이대 분포 통계를 조회한다")
+    @Test
+    public void getUserCountByEachAge() {
+        //given
+        User user1 = User.builder()
+                .email("test1@sluv.com")
+                .snsType(SnsType.KAKAO)
+                .userStatus(ACTIVE)
+                .ageRange(UserAge.TEENAGERS)
+                .build();
+
+        User user2 = User.builder()
+                .email("test2@sluv.com")
+                .snsType(SnsType.KAKAO)
+                .userStatus(ACTIVE)
+                .ageRange(UserAge.TEENAGERS)
+                .build();
+
+        User user3 = User.builder()
+                .email("test3@sluv.com")
+                .snsType(SnsType.KAKAO)
+                .userStatus(ACTIVE)
+                .ageRange(UserAge.TEENAGERS)
+                .build();
+
+        User user4 = User.builder()
+                .email("test4@sluv.com")
+                .snsType(SnsType.KAKAO)
+                .userStatus(ACTIVE)
+                .ageRange(UserAge.TWENTIES)
+                .build();
+
+        User user5 = User.builder()
+                .email("test5@sluv.com")
+                .snsType(SnsType.KAKAO)
+                .userStatus(ACTIVE)
+                .ageRange(UserAge.TWENTIES)
+                .build();
+
+        User user6 = User.builder()
+                .email("test6@sluv.com")
+                .snsType(SnsType.KAKAO)
+                .userStatus(ACTIVE)
+                .ageRange(UserAge.UNKNOWN)
+                .build();
+
+        List<User> users = List.of(user1, user2, user3, user4, user5, user6);
+        userRepository.saveAll(users);
+
+        //when
+        UserCountByCategoryResDto userCountByAge = userService.getUserCountByAge();
+        List<UserCountByEachCategoryResDto> teenagerUsers = userCountByAge.getEachCategory().stream()
+                .filter(dto -> dto.getCategory().equals(UserAge.TEENAGERS.toString()))
+                .toList();
+
+        //then
+        assertThat(userCountByAge.getTotalCount()).isEqualTo(6);
+        assertThat(teenagerUsers.get(0).getCount()).isEqualTo(3);
+        assertThat(teenagerUsers.get(0).getPercent()).isEqualTo(50);
 
     }
 }
